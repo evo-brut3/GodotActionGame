ed77d0e0cf45289bcbb2be233073093766abe0a8
evo-brut3 <kale2134+1@gmail.com>
2021-08-14 15:34:56 +0200

    Add enemy death effect + code refactor
     * Add inheritance to Effect script
     * Add enemy death animation
     * Optimization fix in Grass.gd to preload effect instead of loading it
       everytime a new istance of grass is created
     * Fix player blend positions

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
+const GrassEffect = preload("res://Effects/GrassEffect.tscn")
+
 func create_grass_effect():
-		var GrassEffect = load("res://Effects/GrassEffect.tscn")
 		var grassEffect = GrassEffect.instance()
-		var world = get_tree().current_scene
 		grassEffect.global_position = global_position
-		world.add_child(grassEffect)
+		get_parent().add_child(grassEffect)
 
 func _on_Hurtbox_area_entered(area):
 	create_grass_effect()
