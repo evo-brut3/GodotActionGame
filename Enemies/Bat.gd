fe7562373c2c74f8f19f2cfb00feda9fa200e7e2
evo-brut3 <kale2134+1@gmail.com>
2021-08-22 00:53:46 +0200

    Add wander state to enemy

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -5,6 +5,7 @@ const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 export var ACCELERATION = 300
 export var MAX_SPEED = 50
 export var FRICTION = 200
+export var WANDER_TARGET_RANGE = 4
 
 enum {
 	IDLE,
@@ -22,8 +23,10 @@ onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var hurtbox = $Hurtbox
 onready var softCollision = $SoftCollision
+onready var wanderController = $WanderController
 
 func _ready():
+	state = pick_random_state([IDLE, WANDER])
 	sprite.playing = true
 
 func _physics_process(delta):
@@ -35,25 +38,47 @@ func _physics_process(delta):
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
 			
+			if wanderController.get_time_left() == 0:
+				update_wander()
+				
 		WANDER:
-			pass
+			seek_player()
 			
+			if wanderController.get_time_left() == 0:
+				update_wander()
+			
+			accelerate_towards_point(wanderController.target_position, delta)
+			
+			if global_position.distance_to(wanderController.target_position) <= MAX_SPEED * delta:
+				update_wander()
+				
 		CHASE:
 			var player = playerDetectionZone.player
 			if player != null:
-				var direction = position.direction_to(player.global_position)
-				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+				accelerate_towards_point(player.global_position, delta)
 			else:
 				state = IDLE	
-			sprite.flip_h = velocity.x < 0
 			
 	if softCollision.is_colliding():
 		velocity += softCollision.get_push_vector() * delta * 400
 	velocity = move_and_slide(velocity)
 
+func accelerate_towards_point(point, delta: float):
+	var direction = position.direction_to(point)
+	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+	sprite.flip_h = velocity.x < 0
+	
 func seek_player():
 	if playerDetectionZone.can_see_player():
 		state = CHASE
+
+func update_wander():
+	state = pick_random_state([IDLE, WANDER])
+	wanderController.start_wander_timer(rand_range(1, 3))
+
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
