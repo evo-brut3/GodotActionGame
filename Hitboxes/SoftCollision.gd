c1e10fd095b03e2534cd411b967c7e5afc660339
evo-brut3 <kale2134+1@gmail.com>
2021-08-21 22:50:49 +0200

    Remove redundant func in SoftCollision

--- Hitboxes/SoftCollision.gd
+++ Hitboxes/SoftCollision.gd
@@ -11,6 +11,5 @@ func get_push_vector():
 	if is_colliding():
 		var area = areas[0]
 		push_vector = area.global_position.direction_to(global_position)
-		push_vector = push_vector.normalized()
 	
 	return push_vector
