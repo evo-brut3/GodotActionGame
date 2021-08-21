670d7aa5e46ad447817315e3fb25c309767e188c
evo-brut3 <kale2134+1@gmail.com>
2021-08-21 23:11:44 +0200

    Fix invincibility issue
    Player after being hit by an enemy should have had
    invincibility for some time. Because of the wrong
    parameter switch, this functionality didn't work.

--- Hitboxes/Hurtbox.gd
+++ Hitboxes/Hurtbox.gd
@@ -30,7 +30,7 @@ func _on_Timer_timeout():
 	self.invincible = false
 
 func _on_Hurtbox_invincibility_started():
-	set_deferred("monitorable", false)
+	set_deferred("monitoring", false)
 	
 func _on_Hurtbox_invincibility_ended():
-	monitorable = true
+	monitoring = true
